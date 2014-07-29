2423c1c9383a2557ead33c67990b4f22f1f125be
Matteo Croce <matteo@openwrt.org>
2014-07-30 00:37:47 +0200

    platform device

--- buzzer.c
+++ buzzer.c
@@ -1,7 +1,14 @@
 /*
- * Buzzer driver.
+ * Buzzer driver
  *
  * Copyright (C) 2014 Matteo Croce
+ * 
+ * usage: echo milliseconds frequency >/sys/devices/platform/pcspkr/buzzer
+ * if frequency is omitted default is 1000 Hz
+ * 
+ * eg:
+ * echo 100 440 >/sys/devices/platform/pcspkr/buzzer
+ * echo 100 >/sys/devices/platform/pcspkr/buzzer
  */
 
 /*
@@ -13,22 +20,24 @@
 #include <linux/timex.h>
 #include <linux/module.h>
 #include <linux/sysfs.h>
-#include <linux/device.h>
+#include <linux/platform_device.h>
 
-static ssize_t buzz(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+void buzz(int ms, int hz)
 {
-	int ms;
-	int hz = 1000;
-	int c;
+	int count = 0;
 
-	sscanf(buf, "%u %u", &ms, &hz);
-	c = PIT_TICK_RATE / hz;
+	if (hz < 20 || hz > 20000) {
+		outb(inb_p(0x61) & 0xFC, 0x61);
+		return;
+	}
+
+	count = PIT_TICK_RATE / hz;
 
 	/* set buzzer */
 	outb_p(0xB6, 0x43);
 	/* select desired HZ */
-	outb_p(c & 0xff, 0x42);
-	outb((c >> 8) & 0xff, 0x42);
+	outb_p(count & 0xff, 0x42);
+	outb((count >> 8) & 0xff, 0x42);
 
 	/* start beep */
 	outb_p(inb_p(0x61) | 3, 0x61);
@@ -37,30 +46,26 @@ static ssize_t buzz(struct device *dev, struct device_attribute *attr, const cha
 
 	/* stop beep */
 	outb(inb_p(0x61) & 0xFC, 0x61);
+}
 
-	printk(KERN_DEBUG "buzzer: %dms %dhz\n", ms, hz);
+static ssize_t sysfsbuzz(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	int ms;
+	int hz = 1000;
+
+	sscanf(buf, "%u %u", &ms, &hz);
+
+	buzz(ms, hz);
 
 	return count;
 }
 
-static struct class *buzzer_class;
-static struct device *dev;
+static DEVICE_ATTR(buzzer, 0222, NULL, sysfsbuzz);
 
-static DEVICE_ATTR(buzzer, 0222, NULL, buzz);
-
-static int __init buzzer_init(void)
+static int buzzer_probe(struct platform_device *dev)
 {
-	int err;
+	int err = device_create_file(&dev->dev, &dev_attr_buzzer);
 
-	buzzer_class = class_create(THIS_MODULE, "buzzer");
-	if(!buzzer_class)
-		return -1;
-
-	dev = device_create(buzzer_class, NULL, 260, NULL, "buzzer");
-	if(!dev)
-		return -1;
-
-	err = device_create_file(dev, &dev_attr_buzzer);
 	if (err < 0)
 		return err;
 
@@ -69,17 +74,30 @@ static int __init buzzer_init(void)
 	return 0;
 }
 
-static void __exit buzzer_exit(void)
+static int buzzer_remove(struct platform_device *dev)
 {
-	device_remove_file(dev, &dev_attr_buzzer);
-	device_destroy(buzzer_class, 260);
-	class_destroy(buzzer_class);
+	device_remove_file(&dev->dev, &dev_attr_buzzer);
+	buzz(0, 0);
+	printk(KERN_INFO "buzzer: driver unloaded\n");
 
-	printk(KERN_INFO "buzzer: driver unloaded\n");	
+	return 0;
 }
 
-module_init(buzzer_init);
-module_exit(buzzer_exit);
+static void buzzer_shutdown(struct platform_device *dev)
+{
+	buzz(0, 0);
+}
+
+static struct platform_driver buzzer_platform_driver = {
+	.driver		= {
+		.name	= "pcspkr",
+		.owner	= THIS_MODULE,
+	},
+	.probe		= buzzer_probe,
+	.remove		= buzzer_remove,
+	.shutdown	= buzzer_shutdown,
+};
+module_platform_driver(buzzer_platform_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Matteo Croce");
